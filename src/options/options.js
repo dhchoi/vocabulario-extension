6ff4a34c35d4bc9a839ea8b6745ea9ee1089bda3
Andy Choi <andy.dh.choi@gmail.com>
2015-10-24 23:04:19 -0400

    cleanup options page code and style

--- src/options/options.js
+++ src/options/options.js
@@ -1,27 +1,26 @@
 var triggerKeySelectionId = "trigger-key";
-var saveBtnId = "save";
-var statusId = "status";
+var saveBtnId = "save-btn";
+var saveStatusTextId = "save-status-text";
 
 // restores select box and checkbox state using the preferences stored in chrome.storage
 document.addEventListener('DOMContentLoaded', function () {
-    // Using default values
-    chrome.storage.sync.get({
-        triggerKey: 'none'
-    }, function(items) {
-        document.getElementById(triggerKeySelectionId).value = items.triggerKey;
-    });
+  chrome.storage.sync.get({
+    triggerKey: 'none' // Using default values
+  }, function (items) {
+    document.getElementById(triggerKeySelectionId).value = items.triggerKey;
+  });
 });
 
 // saves options to chrome.storage.sync
 document.getElementById(saveBtnId).addEventListener('click', function () {
-    chrome.storage.sync.set({
-        triggerKey: document.getElementById(triggerKeySelectionId).value
-    }, function() {
-        // Update status to let user know options were saved.
-        var status = document.getElementById(statusId);
-        status.textContent = 'Options saved.';
-        setTimeout(function() {
-            status.textContent = '';
-        }, 750);
-    });
+  chrome.storage.sync.set({
+    triggerKey: document.getElementById(triggerKeySelectionId).value
+  }, function () {
+    // Update status to let user know options were saved.
+    var status = document.getElementById(saveStatusTextId);
+    status.textContent = 'Options saved.';
+    setTimeout(function () {
+      status.textContent = '';
+    }, 750);
+  });
 });
